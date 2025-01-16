03a3e1ea2efaa51c9be8485a7599d8b366eedec1
Yusuf <ybedelci@gmail.com>
2025-01-16 21:03:28 +0100

    adding the files

---
+++ assignment.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# TODO Write down your name and studentnumber of the author(s)
+# Markus Persson (1234567) and Rick Games (1000903)
+
+# Global variables
+# TODO Define (only) the variables which require global scope
+
+
+# INSTALL
+
+# TODO complete the implementation of this function
+function install() {
+    # Do NOT remove next line!    
+    echo "function install"
+
+    # TODO if something goes wrong then call function handle_error
+}
+
+
+# CONFIGURATION
+
+# TODO complete the implementation of this function
+function setup() {
+    # Do NOT remove next line!
+    echo "function setup"
+
+    # TODO if something goes wrong then call function handle_error
+}
+
+
+
+# ERROR HANDLING
+
+# TODO complete the implementation of this function
+function handle_error() {
+    # Do NOT remove next line!
+    echo "function handle_error"
+
+    # TODO print a specific error message
+    # TODO exit this function with a proper integer value.
+
+}
+
+# TODO complete the implementation of this function
+function dequeue() {
+    # Do NOT remove next line!
+    echo "function pop dequeue"
+
+    # TODO if something goes wrong then call function handle_error
+
+}
+
+# TODO complete the implementation of this function
+function enqueue {
+    # Do NOT remove next line!
+    echo "function rollback_spigotserver enqueue"
+
+    # TODO if something goes wrong then call function handle_error
+}
+
+
+# UNINSTALL
+
+# TODO complete the implementation of this function
+function uninstall() {
+    # Do NOT remove next line!
+    echo "function uninstall"  
+
+    # TODO if something goes wrong then call function handle_error
+}
+
+
+
+function main() {
+    # Do NOT remove next line!
+    echo "function main"
+
+
+}
+
+# Do NOT remove next line!
+main "$@"
